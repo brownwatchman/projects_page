c4b8fd09fd883dcd0960b5850c0271d70b57686c
brownwatchman <michaelalexandergiron@gmail.com>
2023-08-28 18:38:12 -0600

    Added more project templates, cards and reworked the site.

---
+++ script.js
@@ -0,0 +1,6 @@
+let currentYear = new Date().getFullYear();
+let year = document.querySelector("#year");
+let currentYearSpan = document.querySelector("#currentYear");
+
+year.textContent = currentYear;
+currentYearSpan.textContent = currentYear;
